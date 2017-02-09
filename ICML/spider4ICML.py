3320dc6f257af4c637f509bd4601635792612429
TuringMacLee <turingmac@hotmail.com>
2017-02-09 15:55:46 +0800

    Speed up for dblp

--- ICML/spider4ICML.py
+++ ICML/spider4ICML.py
@@ -3,20 +3,21 @@ import re
 from operator import itemgetter
 
 sourceUrl = "http://dblp.uni-trier.de/db/conf/icml/"
-years = range(1993, 2017)
+years = range(2016, 2004, -1)
 authorRecord = {}
 
 
 def getHtml(url):
     page = urllib.urlopen(url)
     html = page.read()
+    page.close()
     return html
 
 
 def getLink():
     for year in years:
         link = "http://dblp.uni-trier.de/db/conf/icml/icml" + year.__str__() + ".html"
-        print "Fetching " + link + "......"
+        print "Fetching " + link + "......",
         getAuthor(link)
 
 
@@ -24,7 +25,9 @@ def getAuthor(url):
     html = getHtml(url)
     reg = r'span itemprop="name">.+?</span>'
     authors = re.compile(reg)
-    for author in authors.findall(html):
+    authorList = authors.findall(html)
+    print authorList.__len__()
+    for author in authorList:
         author = author.split('<')[0].split('>')[1]
         authorRecord[author] = authorRecord.get(author, 0) + 1
 
