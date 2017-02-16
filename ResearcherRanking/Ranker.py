12876583b466dbd2e144d1cb9d6098fb2a8559be
TuringMacLee <turingmac@hotmail.com>
2017-02-16 13:28:36 +0800

    Add mark by citation and hIndex

--- ResearcherRanking/Ranker.py
+++ ResearcherRanking/Ranker.py
@@ -1,5 +1,4 @@
 import scholarly
-from operator import itemgetter
 
 fileName = "Authors"
 nameIn = open(fileName + ".txt", 'r')
@@ -8,6 +7,8 @@ names = []
 citation = {}
 hIndex = {}
 counter = 0
+maxCitation = 0
+maxHIndex = 0
 for name in nameIn:
     name = name.strip()
     names.append(name)
@@ -21,20 +22,20 @@ for name in names:
         soup = scholarly._get_soup(scholarly._HOST + url)
         index = soup.find_all('td', class_='gsc_rsb_std')
         h = int(index[2].text)
-        print name.strip() + ": " + str(author.citedby), h,
+        print name.strip(), ": ", author.citedby, h,
         citation[name] = author.citedby
         hIndex[name] = h
+        maxCitation = max(maxCitation, author.citedby)
+        maxHIndex = max(maxHIndex, h)
     except StopIteration:
         print "** " + name.strip() + " Not found!",
         citation[name] = 0
         pass
     counter += 1
     print "\tNo." + str(counter) + '/' + str(length)
-    break
-#citationSorted = sorted(citation.items(), key=itemgetter(1), reverse=True)
-#citation = sorted(citation)
 for name in names:
     try:
-        citationOut.write("%s\t%s\t%s\n" % (name, citation[name], hIndex[name]))
+        mark = (1.0 * citation[name] / maxCitation + 1.0 * hIndex[name] / maxHIndex) / 2
+        citationOut.write("%s\t%d\t%d\t%lf\n" % (name, citation[name], hIndex[name], mark))
     except KeyError:
         pass
